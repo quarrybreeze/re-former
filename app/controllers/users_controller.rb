618fa711db4126111219e5cc18f85ae0f679f4f8
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-23 02:43:39 -0700

    Implement user form with form_with helper and initialize user instance in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   private 
-  
+
   def user_params
     params.expect(user: [:username, :email, :password])
   end

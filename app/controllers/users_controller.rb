e13fa85f1e65b8dcdbc4dbb518ec1f343577fbf1
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-23 03:41:11 -0700

    Add edit and update actions to UsersController with corresponding view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
